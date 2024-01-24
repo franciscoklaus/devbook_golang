d1f036c84c2f86899182dc1c642993a1ba8ac293
Francisco Klaus <franciscoklausfilho@hotmail.com>
2024-01-24 16:51:40 +0000

    atualizando aplicação

---
+++ api/src/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+var (
+	// StringConexaoBanco é a string de conexao com o MYSQL
+	StringConexaoBanco = ""
+
+	// Porta onde a API vai estar rodando
+	Porta = 0
+)
+
+// Carregar vai inicializar as variáveis de ambiente
+func Carregar() {
+
+}
